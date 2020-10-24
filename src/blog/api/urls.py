94c0704a4f0409b54b08fa09471e23bf570e758a
shubhampcvn <shubhampcvn@gmail.com>
2020-10-24 13:33:52 +0530

    Adding Pagination and fetch list of post using class based API view

--- src/blog/api/urls.py
+++ src/blog/api/urls.py
@@ -4,6 +4,7 @@ from blog.api.views import (
     api_update_blog_view,
     api_delete_blog_view,
     api_create_blog_view,
+    ApiBlogListView
 )
 
 app_name = 'blog'
@@ -12,6 +13,8 @@ urlpatterns = [
     path('<slug>/', api_detail_blog_view, name='detail'),
     path('<slug>/update', api_update_blog_view, name='update'),
     path('<slug>/delete', api_delete_blog_view, name='delete'),
-    path('create', api_create_blog_view, name='create')
+    path('create', api_create_blog_view, name='create'),
+    path('list', ApiBlogListView.as_view(), name='list')
+
 
 ]
